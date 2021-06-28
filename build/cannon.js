0d2a90258e58e1d24efc6ef735cd9396dc1c7c11
jiaxin.lai <goodlaijiaxin@qq.com>
2021-06-28 11:44:25 +0800

    1.2.7

--- build/cannon.js
+++ build/cannon.js
@@ -1,4 +1,4 @@
-// Wed, 09 Jun 2021 09:12:09 GMT
+// Mon, 28 Jun 2021 03:43:09 GMT
 
 /*
  * Copyright (c) 2015 cannon.js Authors
@@ -26,7 +26,7 @@
 !function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.CANNON=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
 module.exports={
   "name": "@cocos/cannon",
-  "version": "1.2.6-exp.2",
+  "version": "1.2.7",
   "description": "A lightweight 3D physics engine written in JavaScript.",
   "homepage": "https://github.com/cocos-creator/cannon.js",
   "author": "Stefan Hedman <schteppe@gmail.com> (http://steffe.se), JayceLai",
@@ -14166,6 +14166,7 @@ World.idToBodyMap = {};
 World.idToShapeMap = {};
 World.integrateKinematic = false;
 World.ccdSphereAdvance = false;
+World.autoWakeUpDynamicWhenExit = true;
 World.prototype = new EventTarget();
 
 // Temp stuff
@@ -15122,6 +15123,12 @@ World.prototype.emitCollisionEvents = function () {
             if (this.cm.get(bi, bj)) {
                 if (!bi.isSleeping() || !bj.isSleeping()) {
                     this.cm.set(bi, bj, false);
+
+                    // This is very useful when gravity is not enough to re-awaken the dynamic body
+                    if (World.autoWakeUpDynamicWhenExit) {
+                        if (bi.type & Body.DYNAMIC) bi.wakeUp();
+                        if (bj.type & Body.DYNAMIC) bj.wakeUp();
+                    }
 
                     cc_collide.bi = bi;    
                     cc_collide.contact = data;
